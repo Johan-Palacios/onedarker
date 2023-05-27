cc9196fa825028dbff1d6050f257acffde646db5
Johan Palacios <johanpalacios.rivera@gmail.com>
2023-05-26 18:03:16 -0600

    feat: Update `build.sh`

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@ error() {
     echo $1
     exit 1
 }
-
-if [ -f "./kdfjsal.lua" ]; then
-  colorgen-nvim user_template.toml && mv ./onedarker/palette.lua ./lua/onedarker/palette.lua && rm -r ./onedarker
-else
+if [ ! -f "$FILE" ]; then
   error "File not exist"
+fi
+
+if [ -f "$FILE" ]; then
+  colorgen-nvim user_template.toml && mv ./onedarker/lua/onedarker/palette.lua ./lua/onedarker/palette.lua && rm -r ./onedarker
 fi
